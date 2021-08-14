23ece4259246eddc0efada997def68243041b0f9
Harsh vardhan pujari <64607647+Harshvp412@users.noreply.github.com>
2021-08-14 14:06:25 +0530

    deploy

--- assets/sw.js
+++ assets/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e=>{
     e.waitUntil(
         caches.open("static").then(cache =>{
-            console.log("Installed")
+            console.log("Install")
             return cache.addAll(["./", "/images/logo192.png"]);
            
         })
